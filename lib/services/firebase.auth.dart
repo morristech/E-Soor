2a7aee5fa58734e9b335ae2a21d7f5dd7cf886ed
AhmedAbouelkher <am303737@gmail.com>
2020-05-11 04:13:59 +0200

    omitting API files

--- lib/services/firebase.auth.dart
+++ lib/services/firebase.auth.dart
@@ -1,4 +1,4 @@
-import 'package:E_Soor/services/api.keys.dart';
+import 'package:E_Soor/services/API.keys.dart';
 import 'package:E_Soor/services/users.api.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
