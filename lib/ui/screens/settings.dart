23173b19ecbdaa3e7a7f1d41395ffe9c140207a3
Omar Yehia <omaryahiatawfiek@gmail.com>
2020-05-12 11:02:47 +0200

    Adding Trailing commas in settings file

--- lib/ui/screens/settings.dart
+++ lib/ui/screens/settings.dart
@@ -1,119 +1,117 @@
 import 'package:flutter/material.dart';
+
 class Settings extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(
-                title: Text("Settings"),
-            ),
-            body:ListView(
-              children:<Widget>[
-                Column(
-                children: <Widget>[
-                  Container(
-                    child: ListTile(
-                       title: Text("Account Settings", style:TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Container(
+                child: ListTile(
+                  title: Text(
+                    "Account Settings",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
+                ),
+              ),
 
-                  ///Username
+              ///Username
 
-                  Container(
-                    decoration: BoxDecoration(
-                     border: Border(
-                       bottom: BorderSide(
-                         color: Colors.grey[600],
-                         width: 0.5
-                       )
-                     ),
-                     color: Colors.grey[900],
-                    ),
-                    child:ListTile(
-                      title: Text("Username"),
-                      onTap: (){},
-                    )
-                  ),
-
-                  //Phone Number
-
-                Container(
-                  decoration: BoxDecoration(
-                   border: Border(
+              Container(
+                decoration: BoxDecoration(
+                  border: Border(
                     bottom: BorderSide(
-                         color: Colors.grey[600],
-                         width: 0.5
-                       ),
-                   ),
-                    color: Colors.grey[900]
-                  ),
-                  child: ListTile(
-                    title: Text("Phone "),
-                    onTap: (){},
-                  ),
-                ),
-
-                 ///E-mail address
-
-
-                Container(
-                    decoration: BoxDecoration(
-                     border: Border(
-                       bottom: BorderSide(
-                         color: Colors.grey[600],
-                         width: 0.5
-                       )
-                     ),
-                     color: Colors.grey[900],
+                      color: Colors.grey[600],
+                      width: 0.5,
                     ),
-                  
-                  child: ListTile(
-                     title: Text("Email address"),
-                     onTap: (){},
                   ),
+                  color: Colors.grey[900],
                 ),
-
-                 ///Password 
-
-
-                Container(
-                    decoration: BoxDecoration(
-                     border: Border(
-                       bottom: BorderSide(
-                         color: Colors.grey[600],
-                         width: 0.5
-                       )
-                     ),
-                     color: Colors.grey[900],
-                    ),
-                  
-                  child: ListTile(
-                    title: Text( "Password"),
-                    onTap: (){},
-                  ),
+                child: ListTile(
+                  title: Text("Username"),
+                  onTap: () {},
                 ),
+              ),
 
-                 ///Deactivate Account
+              //Phone Number
 
-                Container(
-                    decoration: BoxDecoration(
-                     border: Border(
-                       bottom: BorderSide(
-                         color: Colors.grey[600],
-                         width: 0.5
-                       )
-                     ),
-                     color: Colors.grey[900],
+              Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.grey[600],
+                      width: 0.5,
                     ),
-                  
-                  child: ListTile(
-                    title: Text( "Deactivate your acccount"),
-                    onTap: (){},
                   ),
-                )
-                ]
-              )
-              ]
-            )
-        );
-    }
+                  color: Colors.grey[900],
+                ),
+                child: ListTile(
+                  title: Text("Phone "),
+                  onTap: () {},
+                ),
+              ),
+
+              ///E-mail address
+
+              Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.grey[600],
+                      width: 0.5,
+                    ),
+                  ),
+                  color: Colors.grey[900],
+                ),
+                child: ListTile(
+                  title: Text("Email address"),
+                  onTap: () {},
+                ),
+              ),
+
+              ///Password
+
+              Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                      bottom: BorderSide(color: Colors.grey[600], width: 0.5)),
+                  color: Colors.grey[900],
+                ),
+                child: ListTile(
+                  title: Text("Password"),
+                  onTap: () {},
+                ),
+              ),
+
+              ///Deactivate Account
+
+              Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.grey[600],
+                      width: 0.5,
+                    ),
+                  ),
+                  color: Colors.grey[900],
+                ),
+                child: ListTile(
+                  title: Text("Deactivate your acccount"),
+                  onTap: () {},
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
